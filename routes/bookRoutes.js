4b657993cfdf729f76590af2d18e3047a32c99a1
Hany Kamal <hanykamal.eg333@gmail.com>
2026-04-26 22:05:42 +0300

    Change PUT to PATCH

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/', getAllBooks);
 router.get('/:id', getBookById);
 router.post('/', createBook);
-router.put('/:id', updateBook);
+router.patch('/:id', updateBook);
 router.delete('/:id', deleteBook);
 
 export default router;
